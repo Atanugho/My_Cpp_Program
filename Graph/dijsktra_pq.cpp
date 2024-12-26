985e4c93fab9179fc3283ae1c1bb1bb4d9b55c7c
Atanugho <ghoshatanu395@gmail.com>
2024-12-26 15:20:41 +0530

    done

--- Graph/dijsktra_pq.cpp
+++ Graph/dijsktra_pq.cpp
@@ -1,36 +1,36 @@
 vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) {
 
-		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-		vector<int> result(V, INT_MAX);
+	vector<int> result(V, INT_MAX);
 
-		result[S] = 0;
-		pq.push({0, S});
-		//NOTE - You can add a visited vector to avoid revisiting a node again and again. It will reduce the time complexity.
+	result[S] = 0;
+	pq.push({0, S});
+	//NOTE - You can add a visited vector to avoid revisiting a node again and again. It will reduce the time complexity.
 
 		while(!pq.empty()) {
 
-		    int d  = pq.top().first;
-		    int node = pq.top().second;
-		    pq.pop();
+			int d  = pq.top().first;
+			int node = pq.top().second;
+			pq.pop();
 
-		    for(auto &vec : adj[node]) {
+			for(auto &vec : adj[node]) {
 
-			int adjNode = vec[0];
-			int dist    = vec[1];
+				int adjNode = vec[0];
+				int dist    = vec[1];
 
-			if(d + dist < result[adjNode]) {
+				if(d + dist < result[adjNode]) {
 
-			    result[adjNode] = d + dist;
-			    pq.push({d+dist, adjNode});
+					result[adjNode] = d + dist;
+					pq.push({d+dist, adjNode});
+
+				}
 
 			}
 
-		    }
-
 		}
 
-		return result;
+	return result;
 
-	    }
+	}
 };
